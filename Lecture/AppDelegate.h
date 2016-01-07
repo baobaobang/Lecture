4f542454972fcc64985ee002639d825b220b16b2
chenxu89 <675269196@qq.com>
2016-01-07 21:45:55 +0800

    Initial Commit

---
+++ Lecture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lecture
+//
+//  Created by 陈旭 on 16/1/7.
+//  Copyright © 2016年 陈旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
