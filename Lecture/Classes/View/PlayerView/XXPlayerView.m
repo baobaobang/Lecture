14618ae903c0130879230b44a4e54b3c1a94b30c
chenxu89 <675269196@qq.com>
2016-01-08 15:49:42 +0800

    完成图片轮播期

--- Lecture/Classes/View/PlayerView/XXPlayerView.m
+++ Lecture/Classes/View/PlayerView/XXPlayerView.m
@@ -8,10 +8,13 @@
 
 #import "XXPlayerView.h"
 
+#define PhotoCount 4
+
 @interface XXPlayerView () <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *pptView;
 @property (weak, nonatomic) IBOutlet UIView *playerToolBar;
 @property (nonatomic, weak) UIScrollView *scrollView;
+@property (nonatomic, strong) NSMutableArray *imageViews;
 @end
 
 
@@ -24,24 +27,46 @@
 // 初始化子控件
 - (instancetype)initWithCoder:(NSCoder *)aDecoder{
     if (self = [super initWithCoder:aDecoder]) {
-        
 
     }
     return self;
 }
 
+
 - (void)awakeFromNib{
+    
     // 1.创建一个scrollView：用于显示所有的ppt图片
     UIScrollView *scrollView = [[UIScrollView alloc] init];
     [self.pptView addSubview:scrollView];
     self.scrollView = scrollView;
+
+//    // 2.添加图片到scrollView中
+//    for (int i = 0; i < PhotoCount; i++) {
+//        UIImageView *imageView = [[UIImageView alloc] init];
+//        NSString *name = [NSString stringWithFormat:@"new_feature_%d", i + 1];
+//        imageView.image = [UIImage imageNamed:name];
+//        [self.scrollView addSubview:imageView];
+//    }
+    
+    // 3.设置scrollView的其他属性
+    // 如果想要某个方向上不能滚动，那么这个方向对应的尺寸数值传0即可
+    scrollView.bounces = NO; // 去除弹簧效果
+    scrollView.pagingEnabled = YES;
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.delegate = self;
+    
+}
+
+- (void)layoutSubviews{
+    [super layoutSubviews];
+    
+    // 设置scrollView的frame
     self.scrollView.frame = self.pptView.bounds;
     
-    // 2.添加图片到scrollView中
+    // 设置imageView的frame
     CGFloat scrollW = self.scrollView.width;
     CGFloat scrollH = self.scrollView.height;
-    NSUInteger photoCount = 4;
-    for (int i = 0; i<photoCount; i++) {
+    for (int i = 0; i<PhotoCount; i++) {
         UIImageView *imageView = [[UIImageView alloc] init];
         imageView.width = scrollW;
         imageView.height = scrollH;
@@ -53,17 +78,8 @@
         [self.scrollView addSubview:imageView];
     }
     
-    // 3.设置scrollView的其他属性
-    // 如果想要某个方向上不能滚动，那么这个方向对应的尺寸数值传0即可
-    scrollView.contentSize = CGSizeMake(photoCount * scrollW, 0);
-    scrollView.bounces = NO; // 去除弹簧效果
-    scrollView.pagingEnabled = YES;
-    scrollView.showsHorizontalScrollIndicator = NO;
-    scrollView.delegate = self;
-}
-
-- (void)layoutSubviews{
-    [super layoutSubviews];
+    
+    self.scrollView.contentSize = CGSizeMake(PhotoCount * scrollW, 0);
 }
 
 
