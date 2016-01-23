50d94ba63cae4eb76c45adc4238e8a66d70a2f02
chenxu89 <675269196@qq.com>
2016-01-23 22:12:07 +0800

    add-添加讲座模型数据

--- Lecture/Classes/Model/XXLecture.h
+++ Lecture/Classes/Model/XXLecture.h
@@ -9,5 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @interface XXLecture : NSObject
+// 讲座id
+@property (nonatomic, copy) NSString *idStr;
+// 讲座介绍图片
 @property (nonatomic, copy) NSString *profilePic;
+// 讲座专家数组
+@property (nonatomic, strong) NSArray *experts;
+// 讲座开始时间
+@property (nonatomic, strong) NSDate *beginTime;
+// 讲座结束时间
+@property (nonatomic, strong) NSDate *endTime;
+// 讲座名字
+@property (nonatomic, copy) NSString *name;
+// 讲座所需积分
+@property (nonatomic, assign) NSUInteger points;
+
 @end
